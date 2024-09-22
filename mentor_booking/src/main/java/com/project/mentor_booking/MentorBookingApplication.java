f04f55295fd42db7b0fc7ae7730539a4b9d4bcbc
thinhdat0104 <nguyenthinhdat09876@gmail.com>
2024-09-22 09:05:31 +0700

    Commit base code - thinhdat0104

---
+++ mentor_booking/src/main/java/com/project/mentor_booking/MentorBookingApplication.java
@@ -0,0 +1,13 @@
+package com.project.mentor_booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentorBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MentorBookingApplication.class, args);
+	}
+
+}
---
+++ mentor_booking/src/test/java/com/project/mentor_booking/MentorBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.mentor_booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentorBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
